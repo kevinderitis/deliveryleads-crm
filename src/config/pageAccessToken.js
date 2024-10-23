6b80db2dad77843dc9be075942b06e378330ac5c
Kevin De Ritis <kevinderitis@MBP-de-Kevin-4865ee1e285f.fibertel.com.ar>
2024-10-23 18:00:30 -0300

    3 fanpage added

--- src/config/pageAccessToken.js
+++ src/config/pageAccessToken.js
@@ -4,4 +4,5 @@ dotenv.config();
 export const pageAccessTokens = {
     '218422194690186': process.env.MESSENGER_ACCESS_TOKEN,
     '482625998261420': process.env.MESSENGER_ACCESS_TOKEN_2,
+    '446049128584514': process.env.MESSENGER_ACCESS_TOKEN_3,
 };
