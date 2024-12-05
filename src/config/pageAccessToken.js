a30cf8c0206d01199d127c841b882fff8c818494
Kevin De Ritis <kevinderitis@MacBook-Pro-de-Kevin.local>
2024-12-05 14:16:15 -0300

    New acces token messenger

--- src/config/pageAccessToken.js
+++ src/config/pageAccessToken.js
@@ -6,4 +6,5 @@ export const pageAccessTokens = {
     '482625998261420': process.env.MESSENGER_ACCESS_TOKEN_2,
     '446049128584514': process.env.MESSENGER_ACCESS_TOKEN_3,
     '192687137270358': process.env.MESSENGER_ACCESS_TOKEN_4,
+    '257561127430271': process.env.MESSENGER_ACCESS_TOKEN_5
 };
